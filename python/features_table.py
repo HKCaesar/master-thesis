9001bb350b54ec99afca8b8b06114c299b4a3e1a
Victor Poughon <victor@pghn.eu>
2015-04-01 14:15:27 +0200

    changer features_table to use 5% outliers level

--- python/features_table.py
+++ python/features_table.py
@@ -7,13 +7,13 @@ import numpy as np
 
 from features_common import base_plot, match_angle, spatial_coverage
 
-def make_features_table(root, subdirs, outlier_threshold):
+def make_features_table(root, subdirs, outlier_threshold, fraction_limit):
     """
     Make the features table for the given image pair directory
     and list of subdirs (from os.walk)
     """
 
-    first_outliers = np.array([])
+    frac_lims = np.array([])
     first_coverages = np.array([])
     # For each algorithm directory
     for subdir in (os.path.join(root, s) for s in subdirs):
@@ -24,16 +24,16 @@ def make_features_table(root, subdirs, outlier_threshold):
         angles = match_angle(matches, shape)
         coverage = spatial_coverage(matches, shape)
 
-        # Indice of first outlier
-        first_outliers = np.append(first_outliers, np.flatnonzero(np.abs(angles) > outlier_threshold)[0])
+        # Indice where outlier fraction > fraction_limit
+        frac_lims = np.append(frac_lims, np.flatnonzero(np.cumsum(np.abs(angles) > outlier_threshold) / angles.size > fraction_limit)[0])
         # Spatial coverage at that indice
-        first_coverages = np.append(first_coverages, coverage[first_outliers[-1]])
+        first_coverages = np.append(first_coverages, coverage[frac_lims[-1]])
 
-    # Sort by descending first_outliers
-    sort_order = np.argsort(first_outliers)[::-1]
+    # Sort by descending frac_lims
+    sort_order = np.argsort(frac_lims)[::-1]
     subdirs = np.array(subdirs, dtype=str)
 
-    return subdirs[sort_order], first_outliers[sort_order], first_coverages[sort_order]
+    return subdirs[sort_order], frac_lims[sort_order], first_coverages[sort_order]
 
 def format_latex_table(algos, outs, covs):
     table = ""
@@ -53,6 +53,6 @@ if __name__ == "__main__":
         if len(subdirs) > 0 and "matches.txt" in os.listdir(os.path.join(root, subdirs[0])):
             # Produce the features table
             f = open(os.path.join(root, "features_table.tex"), "w")
-            f.write(format_latex_table(*make_features_table(root, subdirs, 1.0)))
+            f.write(format_latex_table(*make_features_table(root, subdirs, 1.0, 0.05)))
 
 
