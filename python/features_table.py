32b4bd8bb11ccf91d1d68ec90ce4c507040279ec
Victor Poughon <victor@pghn.eu>
2015-04-01 13:20:59 +0200

    sort features_table by descending first column

--- python/features_table.py
+++ python/features_table.py
@@ -13,8 +13,8 @@ def make_features_table(root, subdirs, outlier_threshold):
     and list of subdirs (from os.walk)
     """
 
-    first_outliers = []
-    first_coverages = []
+    first_outliers = np.array([])
+    first_coverages = np.array([])
     # For each algorithm directory
     for subdir in (os.path.join(root, s) for s in subdirs):
         # Load shape and match data
@@ -25,16 +25,20 @@ def make_features_table(root, subdirs, outlier_threshold):
         coverage = spatial_coverage(matches, shape)
 
         # Indice of first outlier
-        first_outliers.append(np.flatnonzero(np.abs(angles) > outlier_threshold)[0])
+        first_outliers = np.append(first_outliers, np.flatnonzero(np.abs(angles) > outlier_threshold)[0])
         # Spatial coverage at that indice
-        first_coverages.append(coverage[first_outliers[-1]])
+        first_coverages = np.append(first_coverages, coverage[first_outliers[-1]])
 
-    return subdirs, first_outliers, first_coverages
+    # Sort by descending first_outliers
+    sort_order = np.argsort(first_outliers)[::-1]
+    subdirs = np.array(subdirs, dtype=str)
+
+    return subdirs[sort_order], first_outliers[sort_order], first_coverages[sort_order]
 
 def format_latex_table(algos, outs, covs):
     table = ""
     for algo, out, cov in zip(algos, outs, covs):
-        table += "{0} & {1} & {2:0.1f}\\% \\\\\n".format(algo, out, 100*cov)
+        table += "{0} & {1:0.0f} & {2:0.0f} \\\\\n".format(algo, out, 100*cov)
     return table
 
 if __name__ == "__main__":
@@ -48,7 +52,7 @@ if __name__ == "__main__":
         # For each image pair directory
         if len(subdirs) > 0 and "matches.txt" in os.listdir(os.path.join(root, subdirs[0])):
             # Produce the features table
-            f = open(os.path.join(root, "features_list.tex"), "w")
+            f = open(os.path.join(root, "features_table.tex"), "w")
             f.write(format_latex_table(*make_features_table(root, subdirs, 1.0)))
 
 
