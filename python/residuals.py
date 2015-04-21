febbb7bfd1676e257d5f1097940da52958c6cf06
Victor Poughon <victor@pghn.eu>
2015-04-21 16:02:19 +0200

    prototype of python mirror classes for loading project file

--- python/residuals.py
+++ python/residuals.py
@@ -10,27 +10,45 @@ sys.path.append("build")
 try: import pymodel0
 except: print("Error importing pymodel0"); sys.exit(-1)
 
+class Solution(object):
+    def __init__(self, data):
+        self.cameras = np.array(data["cameras"], dtype=np.float64)
+        self.terrain = np.array(data["terrain"], dtype=np.float64)
+
+class Model0(object):
+    def __init__(self, data):
+        self.internal = np.array(data["internal"], dtype=np.float64)
+        self.pixel_size = np.array(data["pixel_size"], dtype=np.float64)
+        self.solutions = [Solution(sol) for sol in data["solutions"]]
+
+class ImageFeatures(object):
+    def __init__(self, data):
+        self.observations = np.array(data["observations"], dtype=np.float64)
+
+
+class Project(object):
+    def __init__(self, filename):
+        p = json.load(open(filename))
+        self.model = Model0(p["model"]["ptr_wrapper"]["data"])
+        self.features = ImageFeatures(p["features"]["ptr_wrapper"]["data"])
+        # self.data_set = DataSet(p["data_set"]["ptr_wrapper"]["data"])
+
 from orthoimage import mm_to_pixels
 
-def compute_residuals(solution, internal, pixel_size, shape, observations):
-    cams = np.array(solution["cameras"], dtype=np.float64)
-    terrain = np.hstack((np.array(solution["terrain"]), np.zeros((len(solution["terrain"]), 1))))
+def compute_residuals(project, solution_number):
+    terrain = np.hstack((project.model.solutions[0].terrain, np.zeros((len(project.model.solutions[0].terrain), 1))))
 
     # in model0 all points are visible in all cams
-    image_pixels = pymodel0.model0_projection_array(internal, cams[0], terrain)
+    image_pixels = pymodel0.model0_projection_array(project.model.internal, project.model.solutions[solution_number].cameras[0], terrain)
     # subtract to features
-    residuals = mm_to_pixels(image_pixels, pixel_size, shape) - observations[:,[0,1]]
+    residuals = mm_to_pixels(image_pixels, project.model.pixel_size, (0,0)) - project.features.observations[:,[0,1]]
     return residuals
 
 def main():
     model_filename = sys.argv[1]
-    project = json.load(open(model_filename))
+    project = Project(model_filename)
 
-    residuals = compute_residuals(project["model"]["ptr_wrapper"]["data"]["solutions"][-1],
-        np.array(project["model"]["ptr_wrapper"]["data"]["internal"]),
-        project["model"]["ptr_wrapper"]["data"]["pixel_size"],
-        (0,0),
-        np.array(project["features"]["ptr_wrapper"]["data"]["observations"]))
+    residuals = compute_residuals(project, -1)
 
     plt.plot(residuals[:,0], residuals[:,1], '+')
     plt.show()
