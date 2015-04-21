8258dc96d5fd5a2947e53ae949a7f281cae7a836
Victor Poughon <victor@pghn.eu>
2015-04-21 13:55:23 +0200

    first try at FlatTile.project_camera

--- cython/pymodel0.pyx
+++ cython/pymodel0.pyx
@@ -10,6 +10,8 @@ cdef extern from "../src/camera_models.h":
 cdef assert_size(np.ndarray[double, ndim=1] arr, long expected_size):
     assert arr.size == expected_size, "Expected array of size {}, got {}".format(expected_size, arr.size)
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
 def model0_projection(np.ndarray[double, ndim=1] internal, np.ndarray[double, ndim=1] external, np.ndarray[double, ndim=1] point):
     assert_size(internal, 3)
     assert_size(external, 6)
@@ -17,6 +19,17 @@ def model0_projection(np.ndarray[double, ndim=1] internal, np.ndarray[double, nd
     cdef double residuals[2]
     model0_projection_double(&internal[0], &external[0], &point[0], residuals)
     return np.array([residuals[0], residuals[1]])
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def model0_projection_array(np.ndarray[double, ndim=1] internal, np.ndarray[double, ndim=1] external, np.ndarray[double, ndim=2] point):
+    assert_size(internal, 3)
+    assert_size(external, 6)
+
+    cdef np.ndarray[double, ndim=2] result = np.zeros((point.shape[0], 2), dtype=np.float64)
+    for i in range(point.shape[0]):
+        model0_projection_double(&internal[0], &external[0], &point[i, 0], &result[i, 0])
+    return result
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
--- python/orthoimage.py
+++ python/orthoimage.py
@@ -27,6 +27,30 @@ class WorldRect(object):
         higher = np.array([points[:,0].max(), points[:,1].max()], dtype=np.float64)
         return cls(lower, higher)
 
+def image_bounds_mask(pixel_points, image_shape):
+    """
+    Returns a numpy mask (array of Bool) of size pixel_points.shape
+    where each columns is True iff the pixel lies wihtin image bounds
+    """
+
+    contained_mask = np.empty(pixel_points.shape, dtype=bool)
+    contained_mask[:,:] = np.all((
+            # 0.5 because nearest neighbour interpolation rounds to closest integer
+            pixel_points[:,0] >= 0,
+            pixel_points[:,0] < (image_shape[0] - 0.5),
+            pixel_points[:,1] >= 0,
+            pixel_points[:,1] < (image_shape[1] - 0.5)
+            ), axis=0)[:,np.newaxis]
+    return contained_mask
+
+def get_pixel_colors(image, pixel_points):
+    "Read image value at float coordinate using nearest neighbour interpolation"
+    indexes = np.asarray(np.round(pixel_points), dtype=int)
+    return image[indexes[:,0], indexes[:,1]]
+
+def mm_to_pixels(pixels, pixel_size, shape):
+    return (pixels / pixel_size)[:, [1, 0]] * np.array([1, -1]) + np.array(shape[:2])/2
+
 class FlatTile(object):
     def __init__(self, rect, gsd):
         margin = gsd*5 # border margin in meters
@@ -41,6 +65,12 @@ class FlatTile(object):
         j = np.round(v[0])
         i = np.round(-v[1])
         return np.array([i, j], dtype=np.long)
+
+    def image_to_world(self, points):
+        "Converts a (n,2) array from image to world coordinates"
+        x = np.array(( points[:,1] * self.gsd) + self.origin[0], dtype=np.float64)
+        y = np.array((-points[:,0] * self.gsd) + self.origin[1], dtype=np.float64)
+        return np.column_stack((x, y))
 
     def draw_world_point(self, point, color):
         i, j = self.world_to_image(point)
@@ -61,8 +91,35 @@ class FlatTile(object):
             self.image[rr[mask], cc[mask], 1] = val[mask] * 255
             self.image[rr[mask], cc[mask], 2] = val[mask] * 255
 
-    def project_one_camera():
-        pass
+    def project_camera(self, internal, external, elevation, pixel_size, image):
+        """
+        Orthorectify one image onto the tile
+        The procedure works with 2D arrays of shape (n, 2),
+        where each represents the same 'point' in different frames.
+        """
+
+        # All pixels in the tile image
+        # 'ij' indexing and reversed i
+        ii, jj = np.meshgrid(np.arange(0, self.image.shape[1], dtype=np.long), np.arange(0, self.image.shape[0], dtype=np.long), indexing="ij")
+        tile_pixels = np.column_stack((ii.flat[::-1], jj.flat))
+
+        # Corresponding ground points in world coordinates
+        ones = elevation*np.ones((tile_pixels.shape[0], 1), dtype=np.float64)
+        world_points = np.copy(np.column_stack((self.image_to_world(tile_pixels), ones)))
+
+        # Project ground points to get pixel coordinates
+        image_pixels = pymodel0.model0_projection_array(internal, external, world_points)
+        print(image_pixels.shape)
+        image_pixels = mm_to_pixels(image_pixels, pixel_size, image.shape)
+
+        # Remove out of bounds pixels
+        mask = image_bounds_mask(image_pixels, self.image.shape)
+        image_pixels = image_pixels[mask].reshape((-1, 2))
+        tile_pixels = tile_pixels[mask].reshape((-1, 2))
+
+        # Retrive RGB values and store them
+        pixel_colors = get_pixel_colors(image, image_pixels)
+        self.image[tile_pixels[:,0], tile_pixels[:,1]] = pixel_colors
 
 def project_corners(internal, camera, pixel_size, im_shape, elevation):
     """Project the four corners of an image onto the ground"""
@@ -74,7 +131,7 @@ def project_corners(internal, camera, pixel_size, im_shape, elevation):
         [-pixel_size*rows/2, -pixel_size*cols/2]])
     return np.array([pymodel0.model0_inverse(internal, camera, pix, elevation) for pix in points_image])
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 2:
         print("Usage: ./orthoimage.py <data_root> <model.json>")
         sys.exit(-1)
@@ -93,12 +150,11 @@ if __name__ == "__main__":
 
     # Down project all cameras corners to get the area
     solution_number = -1
-    world_point = np.zeros((len(project["model"]["ptr_wrapper"]["data"]["solutions"][solution_number]["cameras"])*4, 2))
-    cam_left = project["model"]["ptr_wrapper"]["data"]["solutions"][solution_number]["cameras"][0]
-    cam_right = project["model"]["ptr_wrapper"]["data"]["solutions"][solution_number]["cameras"][1]
+    cam_left = np.array(project["model"]["ptr_wrapper"]["data"]["solutions"][solution_number]["cameras"][0], dtype=np.float64)
+    cam_right = np.array(project["model"]["ptr_wrapper"]["data"]["solutions"][solution_number]["cameras"][1], dtype=np.float64)
 
-    corners_left  = project_corners(internal, np.array(cam_left , dtype=np.float64), pixel_size, image_shape, elevation)
-    corners_right = project_corners(internal, np.array(cam_right, dtype=np.float64), pixel_size, image_shape, elevation)
+    corners_left  = project_corners(internal, cam_left , pixel_size, image_shape, elevation)
+    corners_right = project_corners(internal, cam_right, pixel_size, image_shape, elevation)
 
     world_rect = WorldRect.from_points(np.vstack([corners_left, corners_right]))
 
@@ -107,8 +163,13 @@ if __name__ == "__main__":
 
     tile.draw_cam_trace(corners_left)
     tile.draw_cam_trace(corners_right)
+    tile.project_camera(internal, cam_left, elevation, pixel_size, left)
+    tile.project_camera(internal, cam_right, elevation, pixel_size, right)
 
     io.imsave("tile.jpg", tile.image)
+
+if __name__ == "__main__":
+    main()
 
 """
 load project
@@ -118,85 +179,4 @@ for each camera project it down using direct model
 option: for each keypoint down project it using inverse model and draw matches lines
 """
 
-def tile_pixel_grid(tilesize):
-    """All pixel coordinates of the tile image"""
 
-    # 'ij' indexing and reversed i
-    ii, jj = np.meshgrid(np.arange(0, tilesize, dtype=int), np.arange(0, tilesize, dtype=int), indexing="ij")
-    return np.column_stack((ii.flat[::-1], jj.flat))
-
-def xyz_grid(pixel_grid, tile_x, tile_y, zoom, tilesize):
-    """
-    From a list of pixel points, makes the list of XYZ ground points
-    """
-
-    gm = GlobalMercator(tileSize=tilesize)
-    minx, miny, maxx, maxy = gm.TileBounds(tile_x, tile_y, zoom)
-
-    # Meter coordinates are tile origin + pix*gsd, accounting for i axis flip
-    gsd = zoom_to_gsd(zoom)
-    points = np.empty_like(pixel_grid, dtype=np.float64)
-    points[:,0] = minx + pixel_grid[:,1]*gsd
-    points[:,1] = miny + (tilesize-pixel_grid[:,0])*gsd
-
-    return points
-
-class Tile(object):
-    def __init__(self, tx, ty, zoom, tilesize):
-        self.x, self.y = tx, ty
-        self.zoom = zoom
-        self.tilesize = tilesize
-        self.image = np.zeros((tilesize, tilesize, 3), dtype='uint8')
-
-    def project_one_camera(self, internal, external, elevation, zone, im, input_scale):
-        """
-        Orthorectify one image onto the tile
-        The procedure works with 2D arrays of shape (n, 2),
-        where each represents the same 'point' in different frames.
-        """
-
-        # All pixels in the tile image
-        tile_pixels = tile_pixel_grid(self.tilesize)
-
-        # Corresponding ground points in ESPE3857
-        xyz_points = xyz_grid(tile_pixels, self.x, self.y, self.zoom, self.tilesize)
-
-        # Corresponding UTM points
-        pjt = ProjTransform(zone)
-        utm_points = pjt.xyz_to_utm(xyz_points)
-
-        # Project ground points
-        ground_points = np.vstack((utm_points.transpose(), elevation*np.ones((1, utm_points.shape[0]))))
-        image_pixels = camera_project(internal, external, ground_points).transpose()
-
-        # Remove out of bounds pixels
-        mask = image_bounds_mask(image_pixels, im.shape, input_scale)
-        image_pixels = image_pixels[mask].reshape((-1, 2))
-        tile_pixels = tile_pixels[mask].reshape((-1, 2))
-
-        # Retrive RGB values and store them
-        pixel_colors = get_pixel_colors(im, image_pixels, input_scale)
-        self.image[tile_pixels[:,0], tile_pixels[:,1]] = pixel_colors
-
-def get_pixel_colors(image, pixel_points, input_scale):
-    """
-    Read image value at float coordinate using nearest neighbour interpolation
-    """
-    indexes = np.asarray(np.round(pixel_points / float(input_scale)), dtype=int)
-    return image[indexes[:,0], indexes[:,1]]
-
-def image_bounds_mask(pixel_points, image_shape, input_scale):
-    """
-    Returns a numpy mask (array of Bool) of size pixel_points.shape
-    where each columns is True iff the pixel lies wihtin image bounds
-    """
-
-    contained_mask = np.empty(pixel_points.shape, dtype=bool)
-    contained_mask[:,:] = np.all((
-            # 0.5 because nearest neighbour interpolation rounds to closest integer
-            pixel_points[:,0] >= 0,
-            pixel_points[:,0] / input_scale < (image_shape[0] - 0.5),
-            pixel_points[:,1] >= 0,
-            pixel_points[:,1] / input_scale < (image_shape[1] - 0.5)
-            ), axis=0)[:,np.newaxis]
-    return contained_mask
