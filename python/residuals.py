49d032a2eedc101cfc66a0e43846824229e909de
Victor Poughon <victor@pghn.eu>
2015-04-22 12:41:04 +0200

    fixed typo in python/residuals.py

--- python/residuals.py
+++ python/residuals.py
@@ -6,7 +6,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-from orthoimage import mm_to_pixels
+import sensor_types
 
 sys.path.append("build")
 try: import pymodel0
@@ -22,6 +22,8 @@ class Model0(object):
         self.internal = np.array(data["internal"], dtype=np.float64)
         self.pixel_size = np.array(data["pixel_size"], dtype=np.float64)
         self.solutions = [Solution(sol) for sol in data["solutions"]]
+        self.rows = np.array(data["rows"], dtype=np.float64)
+        self.cols = np.array(data["cols"], dtype=np.float64)
 
 class ImageFeatures(object):
     def __init__(self, data):
@@ -39,21 +41,24 @@ class Project(object):
         # self.data_set = DataSet(p["data_set"]["ptr_wrapper"]["data"])
 
 def compute_residuals(project, solution_number):
-    terrain = np.hstack((project.model.solutions[0].terrain, np.zeros((len(project.model.solutions[0].terrain), 1))))
+    terrain = np.hstack((project.model.solutions[solution_number].terrain, np.zeros((len(project.model.solutions[0].terrain), 1))))
 
     # in model0 all points are visible in all cams
     image_pixels = pymodel0.model0_projection_array(project.model.internal, project.model.solutions[solution_number].cameras[0], terrain)
     # subtract to features
-    residuals = mm_to_pixels(image_pixels, project.model.pixel_size, (0,0)) - project.features.observations[:,[0,1]]
+    residuals = sensor_types.sensor_to_pixel(image_pixels, project.model.pixel_size, project.model.rows, project.model.cols) - project.features.observations[:,[0,1]]
     return residuals
 
 def main():
     model_filename = sys.argv[1]
     project = Project(model_filename)
 
-    residuals = compute_residuals(project, -1)
-
+    residuals = compute_residuals(project, 0)
     plt.plot(residuals[:,0], residuals[:,1], '+')
+
+    residuals = compute_residuals(project, -1)
+    plt.plot(residuals[:,0], residuals[:,1], 'x')
+
     plt.show()
 
 if __name__ == "__main__":
