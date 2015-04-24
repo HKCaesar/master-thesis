4834020921cbec61e09f7aed47ed08357d8f43ab
Victor Poughon <victor@pghn.eu>
2015-04-24 13:11:22 +0200

    added log file to run_all.py

--- run_all.py
+++ run_all.py
@@ -2,6 +2,17 @@
 
 import sys
 import os
+import os.path
+
+def system(command):
+    if os.system(command) != 0:
+        raise RuntimeError("Command failed: {}".format(command))
+
+def do(command, project_dir):
+    command = command.format(project_dir)
+    log = os.path.join(project_dir, "log.txt")
+    system('printf "\n$ {}\n" 2>&1 | tee -a {}'.format(command, log))
+    system("time -p {} 2>&1 | tee -a {}".format(command, log))
 
 if __name__ == "__main__":
 
@@ -9,14 +20,16 @@ if __name__ == "__main__":
 
     if command == "all":
         # Compile
-        os.system("mkdir -p build")
-        os.system("cd build; cmake ..")
-        os.system("make -C build/ -s")
+        system("mkdir -p build")
+        system("cd build; cmake ..")
+        system("make -C build/ -s")
 
         # Features analysis
 
         # Geosolve
-        os.system("./build/geosolve ../data ../results/geosolve/model0 base")
-        os.system("./build/geosolve ../data ../results/geosolve/model0 features")
-        os.system("./build/geosolve ../data ../results/geosolve/model0 solve")
-        os.system("./python/orthoimage.py ../data ../results/geosolve/model0")
+        project_dir = "../results/geosolve/model0"
+        system("mkdir -p {}".format(project_dir))
+        do("./build/geosolve ../data {} base", project_dir)
+        do("./build/geosolve ../data {} features", project_dir)
+        do("./build/geosolve ../data {} solve", project_dir)
+        do("./python/orthoimage.py ../data {}", project_dir)
