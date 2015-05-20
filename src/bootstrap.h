3c3152808a5ec4e59902d012f90ef38c07663f4f
Victor Poughon <victor@pghn.eu>
2015-05-20 10:40:56 +0200

    added bootstrap files

---
+++ src/bootstrap.h
@@ -0,0 +1,23 @@
+#ifndef BOOTSTRAP_H
+#define BOOTSTRAP_H
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include "ceres/ceres.h"
+#include <cereal/types/array.hpp>
+#include "data_set.h"
+#include "image_features.h"
+#include "camera_models.h"
+#include "types.h"
+#include "model.h"
+#include "internal.h"
+
+using std::vector;
+using std::array;
+
+// Takes a Model and perform the bootstrap method on it
+class Bootstrap {
+};
+
+#endif
