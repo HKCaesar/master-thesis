8c5e961a6d74d586acb3bf3bebb9d3ce63457f6d
Victor Poughon <victor@pghn.eu>
2015-05-22 10:08:04 +0200

    bootstrap.py saves to project directory

--- python/bootstrap.py
+++ python/bootstrap.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import os.path
+import os
+from os.path import join
 import numpy as np
 import matplotlib.pyplot as plt
 from project import Project
@@ -48,35 +49,33 @@ def main():
         sys.exit(-1)
 
     project_dir = sys.argv[1]
-    project = Project(os.path.join(project_dir, "project.json"))
+    project = Project(join(project_dir, "project.json"))
 
-    for bootstrap in project.bootstraps:
+    for (bootstrap_number, bootstrap) in enumerate(project.bootstraps):
+        boot_dir = os.path.abspath(join(project_dir, "bootstrap{}-{}".format(bootstrap_number, type(bootstrap.base_model).__name__)))
+        os.makedirs(boot_dir, exist_ok=True)
+
         # Covariance matrices
         ext_covs = covariance_externals(bootstrap)
         int_cov = covariance_internals(bootstrap)
 
-        # X, Y, Z
-        for S in ext_covs:
+        # X, Y, Z and angles
+        for (cam_number, S) in enumerate(ext_covs):
             f, ax = plot_covariance_matrix(S[:3, :3])
-            plt.show(f)
-
-        # Angles
-        for S in ext_covs:
+            f.savefig(join(boot_dir, "covariance-cam{}-pos.pdf".format(cam_number)), bbox_inches='tight')
             f, ax = plot_covariance_matrix(S[3:, 3:])
-            plt.show(f)
+            f.savefig(join(boot_dir, "covariance-cam{}-angles.pdf".format(cam_number)), bbox_inches='tight')
 
         # Interior
         f, ax = plot_covariance_matrix(int_cov)
-        plt.show(f)
+        f.savefig(join(boot_dir, "covariance-interior.pdf"), bbox_inches='tight')
 
-        # Scatter plots
-        for cam in extract_cameras(bootstrap):
+        # Scatter and distribution plots
+        for (cam_number, cam) in enumerate(extract_cameras(bootstrap)):
             f, ax = plot_scatter(cam[:,0], cam[:, 1])
-            plt.show(f)
-
-        # Distributions
-        f, ax = plot_distribution(cam[:,2])
-        plt.show(f)
+            f.savefig(join(boot_dir, "cam{}-xy.pdf".format(cam_number)), bbox_inches='tight')
+            f, ax = plot_distribution(cam[:,2])
+            f.savefig(join(boot_dir, "cam{}-z.pdf".format(cam_number)), bbox_inches='tight')
 
 if __name__ == "__main__":
     main()
