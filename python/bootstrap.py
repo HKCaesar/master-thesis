b3f1b0b63221bacbb91e4e39dd4e966c31343b73
Victor Poughon <victor@pghn.eu>
2015-05-22 11:32:20 +0200

    hinton-ish diagram

--- python/bootstrap.py
+++ python/bootstrap.py
@@ -9,20 +9,69 @@ from project import Project
 from features_common import base_plot
 from IPython import embed
 
-def plot_covariance_matrix(S):
-    "Make a covariance matrix plot"
-    f, ax = base_plot()
-    ax.imshow(S, interpolation="nearest", cmap="gray")
+
+def blob(ax, x, y, area, color):
+    if area > 0:
+        hs = np.sqrt(area) / 2
+        xcorners = np.array([x - hs, x + hs, x + hs, x - hs])
+        ycorners = np.array([y - hs, y - hs, y + hs, y + hs])
+        ax.fill(xcorners, ycorners, color=color, edgecolor=color)
+
+def plot_covariance_matrix(S, labels):
+    "Plot a covariance matrix with Hinton-ish diagram"
+    covariance_matrix_font_size = 18
+    f, ax = base_plot(figsize=(4,4))
+    ax.invert_yaxis()
+    # ax.imshow(S, interpolation="nearest", cmap="gray")
+    n = S.shape[0]
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
+    ax.get_xaxis().tick_top()
+    ax.get_xaxis().set_ticks_position('none')
+    ax.get_yaxis().set_ticks_position('none')
+    ax.set_xticks(np.arange(n))
+    ax.set_yticks(np.arange(n))
+    ax.set_xticklabels(labels, fontsize=covariance_matrix_font_size)
+    ax.set_yticklabels(labels, fontsize=covariance_matrix_font_size)
+
+    def format_cov(c):
+        if np.allclose(c, 0.0):
+            return "0"
+        elif np.allclose(c, 1.0):
+            return "1"
+        elif np.allclose(c, -1.0):
+            return "-1"
+        else:
+            return "{0:0.2f}".format(c).replace("-0", "-").lstrip("0")
+
+    def covcolor(c):
+        return [1.0 - abs(c)]*3
+
+    def textcolor_for_cov(c):
+        return "white" if abs(c) > 0.5 else "black"
+
+    for x in range(n):
+        for y in range(n):
+            blob(ax, x, y, abs(S[x, y]), covcolor(S[x,y]))
+            ax.text(x, y, format_cov(S[x, y]),
+                    fontsize=covariance_matrix_font_size,
+                    horizontalalignment="center",
+                    verticalalignment="center",
+                    color=textcolor_for_cov(S[x, y]))
+
     return f, ax
 
 def plot_scatter(X, Y):
     f, ax = base_plot(figsize=(8,4))
     ax.plot(X, Y, '.')
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
     return f, ax
 
 def plot_distribution(X):
     f, ax = base_plot(figsize=(8,4))
-    ax.hist(X)
+    ax.hist(X, normed=True, bins=X.size/6)
+    ax.set_ylabel("Frequency")
     return f, ax
 
 def main():
@@ -39,7 +88,7 @@ def main():
         os.makedirs(boot_dir, exist_ok=True)
 
         # Interior
-        f, ax = plot_covariance_matrix(np.cov(bootstrap.internals, rowvar=0))
+        f, ax = plot_covariance_matrix(np.corrcoef(bootstrap.internals, rowvar=0), ["fx", "fy", "ppx", "ppy", "ps"])
         f.savefig(join(boot_dir, "covariance-interior.pdf"), bbox_inches='tight')
 
         # For each camera
@@ -48,13 +97,14 @@ def main():
             f, ax = plot_scatter(cam[:,0], cam[:, 1])
             f.savefig(join(boot_dir, "cam{}-xy.pdf".format(cam_number)), bbox_inches='tight')
             f, ax = plot_distribution(cam[:,2])
+            ax.set_xlabel("Z")
             f.savefig(join(boot_dir, "cam{}-z.pdf".format(cam_number)), bbox_inches='tight')
 
             # X, Y, Z and angles covariances matrices
-            S = np.cov(cam, rowvar=0)
-            f, ax = plot_covariance_matrix(S[:3, :3])
+            S = np.corrcoef(cam, rowvar=0)
+            f, ax = plot_covariance_matrix(S[:3, :3], ["X", "Y", "Z"])
             f.savefig(join(boot_dir, "covariance-cam{}-pos.pdf".format(cam_number)), bbox_inches='tight')
-            f, ax = plot_covariance_matrix(S[3:, 3:])
+            f, ax = plot_covariance_matrix(S[3:, 3:], ["ang1", "ang2", "ang3"])
             f.savefig(join(boot_dir, "covariance-cam{}-angles.pdf".format(cam_number)), bbox_inches='tight')
 
 if __name__ == "__main__":
